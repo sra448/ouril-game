86f0401a29d310e725d4064f6a26b57294eb2d73
Flo <florian.unternaehrer@simplificator.com>
2018-01-10 22:28:05 +0100

    show the board, no interaction yet

--- app/view.js
+++ app/view.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { connect } from "react-redux"
+import { chunk } from "lodash"
+
+
+require("./style.scss")
 
 
 // React Redux Bindings
 
 const mapStateToProps = (state) => {
-  return state
+  return { state }
 }
 
 
@@ -17,9 +21,30 @@ const mapDispatchToProps = () => {
 // Main Component
 
 
-const main = (state) => {
+const main = ({ state }) => {
+  const board = state.getIn(["board"]).toArray()
+  const [player, opponent] = chunk(board, 6)
+
   return (
-    <h1>ouril</h1>
+    <div>
+      <h1>ouril</h1>
+      <div>
+        <div class="board-side">
+          <div>
+            { opponent.reverse().map((x, i) => (
+              <div key={i}>{x}</div>
+            ))}
+          </div>
+        </div>
+        <div class="board-side">
+          <div>
+            { player.map((x, i) => (
+              <div key={i}>{x}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
